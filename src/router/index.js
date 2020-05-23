32db8d53b84a934a332da78f13254578c412c218
justdheja <justdheja@gmail.com>
2020-05-23 20:19:15 +0700

    2

--- src/router/index.js
+++ src/router/index.js
@@ -11,36 +11,7 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  {
-    path: "/brazil",
-    name: "Brazil",
-    component: () => import(/* webpackChunkName: "Brazil" */'../views/Brazil.vue')
-  },
-  {
-    path: "/jamaica",
-    name: "Jamaica",
-    component: () => import(/* webpackChunkName: "Jamaica" */'../views/Jamaica.vue')
-  },
-  {
-    path: "/panama",
-    name: "Panama",
-    component: () => import(/* webpackChunkName: "Panama" */'../views/Panama.vue')
-  },
-  {
-    path: "/hawaii",
-    name: "Hawaii",
-    component: () => import(/* webpackChunkName: "Hawaii" */'../views/Hawaii.vue')
-  },
-  {
-    path: "/details",
+    path: "/details/:id",
     name: "DestinationDetails",
     component: () => import(/* webpackChunkName: "DestinationDetails" */'../views/DestinationDetails.vue')
   },
--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,8 +1,47 @@
 <template>
-  <div>
-    <h2>
-      The Destination Id : {{ this.$route.params.id }}
-      {{ this.$route.params.name }}
-    </h2>
-  </div>
+  <section class="destination">
+    <h1>
+      {{ destination.name }}
+    </h1>
+    <div class="destination-details">
+      <img :src="require(`@/assets/${destination.image}`)" :alt="destination.name">
+      <p>{{destination.description}}</p>
+    </div>
+  </section>
 </template>
+
+<script>
+import store from '../store'
+export default {
+  data() {
+    return {
+      destinationID:this.$route.params.id
+    }
+  },
+  computed: {
+    destination(){
+      return store.destinations.find(
+        destination => destination.id === this.destinationID
+      )
+    }
+  },
+}
+</script>
+
+<style scoped>
+img {
+  max-width: 600px;
+  height: auto;
+  width: 100%;
+  max-height: 400px;
+}
+.destination-details {
+  display: flex;
+  justify-content: space-between;
+}
+p {
+  margin: 0 40px;
+  font-size: 20px;
+  text-align: left;
+}
+</style>
